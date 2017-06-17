a2de9c221d73be2f903f732d316ce6cc920f545a
alicespyglass <alicespyglass@gmail.com>
2017-06-17 12:12:36 +0100

    q2 select elements starting with vowel complete with test

--- spec/QuestionsSpec.js
+++ spec/QuestionsSpec.js
@@ -9,4 +9,10 @@ describe('Questions', function () {
     const n = ['bananas', 'apples', 'pears', 'avocados'];
     expect(questions.selectElementsStartingWithA(n)).toEqual(['apples', 'avocados']);
   });
+
+  it ('2 - keep only the elements that start with a vowel', function () {
+    const n =  ['john', 'david', 'omar', 'fred', 'idris', 'angela']
+    expect(questions.selectElementsStartingWithVowel(n)).toEqual(['omar', 'idris', 'angela']);
+  });
+
 });
--- src/Questions.js
+++ src/Questions.js
@@ -1,5 +1,6 @@
 function Questions () {
 
+ // 1. keep only the elements that start with an a
   Questions.prototype.selectElementsStartingWithA = function (array) {
     const ans1 = [];
     array.forEach(function (element) {
@@ -10,4 +11,16 @@ function Questions () {
     });
     return ans1;
   };
+
+  // 2. keep only the elements that start with a vowel
+  Questions.prototype.selectElementsStartingWithVowel = function (array) {
+    const ans2 = [];
+    array.forEach(function (element) {
+      if (element.match(/^[aeiou]/i)) {
+        ans2.push(element);
+      }
+    });
+    return ans2
+  };
+
 }
